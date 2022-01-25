d3914fa9d7ad2e68b4a726c13b781b02ba1be486
Junha Park <junha.park@mail.mcgill.ca>
2022-01-25 14:32:04 -0500

    Integrated React

---
+++ frontend/src/components/MainPageTopFixedFrame.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function MainPageTopFixedFrame(){
+    return(
+        <div id="div-main-page-top-fixed-frame">
+            <div id="div-main-page-top-fixed-frame-texts">
+                <a href="/">드림하우스 · 이집트 최대 점유율! 만족도 1위!!</a>
+                <p>연중무휴 · 오전 9시 - 오후 9시 (GMT+2) · 문의 604-893-8687 · 북미 무료전화 1-877-556-8687</p>
+            </div>
+            <div id="div-main-page-top-fixed-frame-btns">
+                <button>로그인</button>
+                <button>예약조회</button>
+                <button>예약문의</button>
+            </div>          
+        </div>
+    );
+}
+
+export default MainPageTopFixedFrame;
--- src/main/js/App.js
+++ src/main/js/App.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
 import ReactDom from 'react-dom';
+import MainPageTopFixedFrame from "../../../frontend/src/components/MainPageTopFixedFrame";
 
 export class App extends Component{
     render(){
         return(
             <div>
+                <MainPageTopFixedFrame/>
                 <h1>Welcome to our Springboot-React Full Stack App</h1>
                 <h1>Adding my line 1</h1>
             </div>
